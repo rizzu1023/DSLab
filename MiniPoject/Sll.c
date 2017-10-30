92f0e8de90e9e827b11dcbe5af63d7f2a1a7c977
Mohammed Rizwan <mrizwan9022@gmail.com>
2017-10-30 18:36:33 +0530

    Done:

---
+++ MiniPoject/Sll.c
@@ -0,0 +1,471 @@
+/*Description:Implementation of LINKEDLIST
+* Learner:Mohammed Rizwan
+ * Roll no:16CO39
+ */
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct linked_list
+{
+	int data;
+	struct linked_list *next;
+}node;
+
+void print(node *q)
+{
+	node *ptr;
+	ptr=q;
+	printf("\n");
+	while(ptr!=NULL)
+	{
+		printf("%d\t",ptr->data);
+		ptr=ptr->next;
+	}
+}
+
+void insbeg(node **q,int no)
+{
+	node *temp,*ptr;
+	temp=*q;
+	ptr=(node*)malloc(sizeof(node));
+	ptr->data=no;
+	if(temp==NULL)
+		ptr->next=NULL;
+	else
+		ptr->next=temp;
+	*q=ptr;
+	printf("\nElements of linked list after insertion\n");
+	print(*q);
+}
+
+void insend(node **q,int no)
+{
+	node *ptr,*temp;
+	ptr=(node*)malloc(sizeof(node));
+	ptr->data=no;
+	ptr->next=NULL;
+	temp=*q;
+	if(temp==NULL)
+		*q=ptr;
+	else
+	{
+		while(temp->next!=NULL)
+			temp=temp->next;
+			temp->next=ptr;
+	}
+	printf("\nElements of Linked list after insertion\n");
+	print(*q);
+}
+
+void insafter(node *q,int no)
+{
+	int loc,k;
+	node *temp,*ptr,*old;
+	temp=q;
+	ptr=(node*)malloc(sizeof(node));
+	ptr->data=no;
+	printf("Enter location where the no is to be inserted: ");
+	scanf("%d",&loc);
+	if(loc==1)
+	{
+		printf("\nPlease use Insert at the beginning option\n");
+	    return;
+	}
+	for(k=1;k<loc;k++)
+	{
+		if(temp==NULL)
+				printf("\nElements are less than provided location\n");
+		else
+		{
+			old=temp;
+			temp=temp->next;
+		}
+	}
+	if(temp==NULL)
+	{	
+		printf("\nPlease use Insert at the end option\n");
+		return;
+	}	
+	ptr->next=temp;
+	old->next=ptr;
+	printf("\nElements of linked list after insertion\n");
+	print(q);
+}
+
+void del(node **q,int no)
+{
+	int f=0;
+	node *old,*temp;
+	temp=*q;
+	while(temp!=NULL)
+	{
+		if(temp->data==no)
+		{
+			f=1;
+			if(temp==*q)
+				*q=temp->next;
+			else
+				old->next=temp->next;
+			free(temp);
+			break;
+		}
+		else
+		{
+			old=temp;
+			temp=temp->next;
+		}
+	}
+	if(f==0)
+		printf("\nThe given no is not found\n");
+	printf("\nElements of linked list after deletion\n");
+	print(*q);
+}
+
+void traverse(node *q)
+{
+	printf("\nTraversing Linklist\n");
+	print(q);
+	printf("\nEnd of linked list\n");
+}
+
+int main()
+{
+	node *start,*ptr,*temp;
+	int i,n,j,no,c;
+	printf("\nEnter the no of nodes: ");
+	scanf("%d",&n);
+	printf("Enter node number 1: ");
+	start=(node*)malloc(sizeof(node));
+	scanf("%d",&start->data);
+	temp=start;
+	for(i=1;i<n;i++)
+	{
+		ptr=(node*)malloc(sizeof(node));
+		printf("\nEnter node number %d: ",i+1);
+		scanf("%d",&ptr->data);
+		temp->next=ptr;
+		temp=ptr;
+	}
+	temp->next=NULL;
+	do
+	{
+		printf("\nEnter your choice\n");
+		printf("\n1-Insertion\n2-Deletion\n3-Traverse\n4-Exit\n\n");
+		scanf("%d",&j);
+		switch(j)
+		{
+			case 1:
+				printf("\nEnter the number to be inserted: ");
+				scanf("%d",&no);
+				printf("\nEnter 1 to insert at the beginning\n");
+				printf("\nEnter 2 to insert at the end\n");
+				printf("\nEnter 3 to insert at a specified location\n");
+				scanf("%d",&c);
+				switch(c)
+				{
+					case 1:
+						insbeg(&start,no);
+						break;
+					case 2:
+						insend(&start,no);
+						break;
+					case 3:
+						insafter(start,no);
+						break;
+					default:
+						printf("\nInvalid choice.");
+						break;
+				}
+				break;
+			case 2:
+				printf("\nEnter the number to be deleted: ");
+				scanf("%d",&no);
+				del(&start,no);
+				break;
+			case 3:
+				traverse(start);
+				break;
+			case 4:
+				exit(0);
+			default:
+				printf("\nInvalid input.");
+				break;
+			}
+		}while(1);
+	return 0;
+}
+
+/*OUTPUT:
+Enter the no of nodes: 5
+Enter node number 1: 11
+
+Enter node number 2: 22
+
+Enter node number 3: 33
+
+Enter node number 4: 44
+
+Enter node number 5: 55
+
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+3
+
+Traversing Linklist
+
+11	22	33	44	55	
+End of linked list
+
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+1
+
+Enter the number to be inserted: 66
+
+Enter 1 to insert at the beginning
+
+Enter 2 to insert at the end
+
+Enter 3 to insert at a specified location
+2
+
+Elements of Linked list after insertion
+
+11	22	33	44	55	66	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+1
+
+Enter the number to be inserted: 9
+
+Enter 1 to insert at the beginning
+
+Enter 2 to insert at the end
+
+Enter 3 to insert at a specified location
+1
+
+Elements of linked list after insertion
+
+9	11	22	33	44	55	66	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+1
+
+Enter the number to be inserted: 25
+
+Enter 1 to insert at the beginning
+
+Enter 2 to insert at the end
+
+Enter 3 to insert at a specified location
+3
+Enter location where the no is to be inserted: 4
+
+Elements of linked list after insertion
+
+9	11	22	25	33	44	55	66	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+1
+
+Enter the number to be inserted: 69
+
+Enter 1 to insert at the beginning
+
+Enter 2 to insert at the end
+
+Enter 3 to insert at a specified location
+3
+Enter location where the no is to be inserted: 9
+
+Please use Insert at the end option
+
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+1
+
+Enter the number to be inserted: 5
+
+Enter 1 to insert at the beginning
+
+Enter 2 to insert at the end
+
+Enter 3 to insert at a specified location
+3
+Enter location where the no is to be inserted: 1
+
+Please use Insert at the beginning option
+
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 66
+
+Elements of linked list after deletion
+
+9	11	22	25	33	44	55	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 22
+
+Elements of linked list after deletion
+
+9	11	25	33	44	55	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 33
+
+Elements of linked list after deletion
+
+9	11	25	44	55	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 9
+
+Elements of linked list after deletion
+
+11	25	44	55	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 11
+
+Elements of linked list after deletion
+
+25	44	55	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 44
+
+Elements of linked list after deletion
+
+25	55	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 55
+
+Elements of linked list after deletion
+
+25	
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 25
+
+Elements of linked list after deletion
+
+
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+2
+
+Enter the number to be deleted: 1
+
+The given no is not found
+
+Elements of linked list after deletion
+
+
+Enter your choice
+
+1-Insertion
+2-Deletion
+3-Traverse
+4-Exit
+
+4
+
+
+------------------
+(program exited with code: 0)
+Press return to continue
+*/
---
+++ MiniPoject/cqueue.c
@@ -0,0 +1,315 @@
+/*Description:Implementation of Circular queue
+* Learner:Mohammed Rizwan
+ * Roll no:16CO39
+ */
+#include<stdio.h>
+#include<math.h>
+#define MAX_SIZE 10
+
+int queue[MAX_SIZE],rear=-1,front=-1;
+
+int isFull()
+{
+//check if queue is full
+	return((rear==MAX_SIZE-1&&front==0)||(rear+1==front));
+}
+
+int isEmpty()
+{
+//check if queue is empty
+	return(rear==-1&&front==-1);
+}
+
+void display()
+{
+	int i=front;
+	if(!isEmpty())
+	{
+		while(i!=rear)
+		{
+			printf("%d\t",queue[i]);
+			i=(i+1)%MAX_SIZE;
+		}
+		printf("%d\t",queue[i]);
+	}
+	else
+	{
+		printf("\nQueue is Empty.");
+	}				
+}
+
+void insertQueue(int e)
+{
+	if(!isFull())
+	{
+		rear=(rear+1)%(MAX_SIZE);
+		queue[rear]=e;
+		if(front==-1)
+			front++;
+		printf("\nElement %d is successfully Inserted",e);
+		printf("\nfront=%d\trear=%d",front,rear);
+	}
+	else
+	{
+		printf("\nThe queue is full..element cannot be inserted\n");
+	}
+}
+
+void deleteQueue()
+{
+	if(!isEmpty())
+	{
+		int d=queue[front];
+		if(front==rear)
+		{
+		front=rear=-1;
+		}
+		else
+		{
+		front=(front+1)%(MAX_SIZE);
+		}
+		printf("\nThe element deleted is %d\n",d);
+		printf("\nfront=%d\trear=%d",front,rear); 
+	}
+	else
+	{
+		printf("\nQueue is empty\n");
+	}
+}
+
+int main(){
+	int choice,e;
+	do
+	{
+		printf("\nEnter\n1.display\t2.insert\t3.delete\t4.exit\n");
+		printf("\nEnter your choice: ");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case 1:
+					printf("\nElements of queue are\n");
+				    display();
+				    break;
+			case 2:
+					printf("\nEnter the element to be inserted: ");
+				    scanf("%d",&e);
+				    insertQueue(e);
+				    break;
+			case 3:
+					deleteQueue();
+				    break;
+			case 4:
+				    return 0;
+			        break;
+			default:
+					printf("\nWrong choice\n");
+			        break;
+		}
+	}while(1);
+	return 0;
+}
+
+/*OUTPUT:
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 10
+
+Element 10 is successfully Inserted
+front=0	rear=0
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 20
+
+Element 20 is successfully Inserted
+front=0	rear=1
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 30
+
+Element 30 is successfully Inserted
+front=0	rear=2
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 40
+
+Element 40 is successfully Inserted
+front=0	rear=3
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 50
+
+Element 50 is successfully Inserted
+front=0	rear=4
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 60
+
+Element 60 is successfully Inserted
+front=0	rear=5
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 70
+
+Element 70 is successfully Inserted
+front=0	rear=6
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 80
+
+Element 80 is successfully Inserted
+front=0	rear=7
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 90
+
+Element 90 is successfully Inserted
+front=0	rear=8
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 100
+
+Element 100 is successfully Inserted
+front=0	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 2
+
+Enter the element to be inserted: 110
+
+The queue is full..element cannot be inserted
+
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 1
+
+Elements of queue are
+10	20	30	40	50	60	70	80	90	100	
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 10
+
+front=1	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 20
+
+front=2	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 30
+
+front=3	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 40
+
+front=4	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 50
+
+front=5	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 60
+
+front=6	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 70
+
+front=7	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 80
+
+front=8	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 90
+
+front=9	rear=9
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+The element deleted is 100
+
+front=-1	rear=-1
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 3
+
+Queue is empty
+
+Enter
+1.display	2.insert	3.delete	4.exit
+
+Enter your choice: 4
+
+
+------------------
+(program exited with code: 0)
+Press return to continue
+*/
